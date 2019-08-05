90a27cf305f0be9e261e19c8b1078bda286f73f5
Kim Joon <jk1094@naver.com>
2019-08-05 23:36:45 +0900

    FileInputStream

--- Socket/src/FileStream.java
+++ Socket/src/FileStream.java
@@ -1,9 +1,20 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
 public class FileStream {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		String Filepath = "/Users/joon/Downloads/sample.mp4";
+		
+		try {
+			FileInputStream is = new FileInputStream(Filepath);
+			System.out.println(is);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
